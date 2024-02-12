5f42d209c78546847e92e67da405497df7699e71
Ziang Xu <xuz218@wfu.edu>
2024-02-11 23:29:01 -0500

    update

---
+++ frontend/src/views/WelcomeView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+        <div style="flex: 1">
+            <el-image style="width: 100%;height: auto" fit="cover"
+                      src="http://xiyanhua125.com/img/img4.02003c2e.jpg"/>
+        </div>
+        <div class="welcome-title">
+
+        </div>
+        <div class="right-card">
+            <router-view v-slot="{ Component }">
+                <transition name="el-fade-in-linear" mode="out-in">
+                    <component :is="Component" style="height: 100%"/>
+                </transition>
+            </router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.right-card {
+    width: 450px;
+    z-index: 1;
+    background-color: var(--el-bg-color);
+}
+
+.welcome-title {
+    position: absolute;
+    bottom: 30px;
+    left: 30px;
+    color: white;
+    text-shadow: 0 0 10px black;
+}
+</style>
